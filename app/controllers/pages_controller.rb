4f7cade5a7d7f818f3a001d3f23d32469232decf
Frknk <frknk1@protonmail.com>
2024-03-13 15:32:52 -0500

    feat: create movies

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,10 @@ class PagesController < ApplicationController
   end
 
   def home
-
+    if !user_signed_in?
+      redirect_to root_path
+    else
+      render layout: "home"
+    end
   end
 end
